a841b92cbf5ab5877b25f2af66b7a9b595bffe1a
lozhenru <zhenru@ndrmedical.com>
2023-05-08 01:30:23 +0800

    added left panel header

--- src/blueprints/content_manager/static/js/content_manager.js
+++ src/blueprints/content_manager/static/js/content_manager.js
@@ -1,11 +1,12 @@
 $(function() {
     $("#content_manager_anchor").toggleClass("current_anchor");
+    $(".panel_header_text").text($(".side_anchor.current_anchor .side_anchor_text").text());
 
     $("#test_form").submit(function(event){
         console.log("Here");
         event.preventDefault();
         $.ajax({
-            url: "/collection/create",
+            url: "/content_manager/create",
             method: "POST",
             data: $(this).serialize(),
             success: function(data) {
