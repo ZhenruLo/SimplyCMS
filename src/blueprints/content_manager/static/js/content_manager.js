b06a105e8decd434b019f639764b5847ac6f4d20
lozhenru <zhenru@ndrmedical.com>
2023-05-10 17:56:37 +0800

    initial left panel tab design

--- src/blueprints/content_manager/static/js/content_manager.js
+++ src/blueprints/content_manager/static/js/content_manager.js
@@ -1,45 +1,51 @@
 $(function() {
+    $(".left_panel_tab").on("click", function(event){
+        event.preventDefault();
+        $(".left_panel_tab").removeClass("selected_tab")
+        $(this).toggleClass("selected_tab");
+    });
+
     $("#content_manager_anchor").toggleClass("current_anchor");
     $(".panel_header_text").text($(".side_anchor.current_anchor .side_anchor_text").text());
 
-    $(".test_button").on("click", function(){
-        $.ajax({
-            url: "/content_manager/databases",
-            method: "GET",
-            success: function(data) {
-                console.log(data["databases"])
-            },
-            error: function(data) {
-                alert(data.responseText);
-            },
-        });
-    });
+    // $(".test_button").on("click", function(){
+    //     $.ajax({
+    //         url: "/content_manager/databases",
+    //         method: "GET",
+    //         success: function(data) {
+    //             console.log(data["databases"])
+    //         },
+    //         error: function(data) {
+    //             alert(data.responseText);
+    //         },
+    //     });
+    // });
 
-    $(".test_button1").on("click", function(){
-        $.ajax({
-            url: "/content_manager/databases",
-            method: "PUT",
-            success: function(data) {
-                console.log(data["database"])
-            },
-            error: function(data) {
-                alert(data.responseText);
-            },
-        });
-    });
+    // $(".test_button1").on("click", function(){
+    //     $.ajax({
+    //         url: "/content_manager/databases",
+    //         method: "PUT",
+    //         success: function(data) {
+    //             console.log(data["database"])
+    //         },
+    //         error: function(data) {
+    //             alert(data.responseText);
+    //         },
+    //     });
+    // });
 
-    $("#test_form").submit(function(event){
-        event.preventDefault();
-        $.ajax({
-            url: "/content_manager/databases",
-            method: "POST",
-            data: $(this).serialize(),
-            success: function(data) {
-                alert(data.msg);
-            },
-            error: function(data) {
-                alert(data.responseText);
-            },
-        });
-    });
+    // $("#test_form").submit(function(event){
+    //     event.preventDefault();
+    //     $.ajax({
+    //         url: "/content_manager/databases",
+    //         method: "POST",
+    //         data: $(this).serialize(),
+    //         success: function(data) {
+    //             alert(data.msg);
+    //         },
+    //         error: function(data) {
+    //             alert(data.responseText);
+    //         },
+    //     });
+    // });
 });
