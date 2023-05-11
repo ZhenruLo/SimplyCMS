a6433068f8f58f1cb1077928309654cc6f1810b3
lozhenru <zhenru@ndrmedical.com>
2023-05-11 08:51:08 +0800

    update leftpanel design

--- src/blueprints/content_manager/static/js/content_manager.js
+++ src/blueprints/content_manager/static/js/content_manager.js
@@ -1,8 +1,19 @@
 $(function() {
+    function open_menu(id){
+        $(".left_panel_content_container").removeClass("selected_panel");
+        if (id === "menu_tab" && $("#"+id).hasClass("selected_tab")){
+            $("#left_panel_menu").toggleClass("selected_panel");
+        }
+        else if (id === "information_tab" && $("#"+id).hasClass("selected_tab")){
+            $("#left_panel_info").toggleClass("selected_panel");
+        }
+    };
+
     $(".left_panel_tab").on("click", function(event){
         event.preventDefault();
         $(".left_panel_tab").removeClass("selected_tab")
         $(this).toggleClass("selected_tab");
+        open_menu($(this).attr("id"));
     });
 
     $("#content_manager_anchor").toggleClass("current_anchor");
