48ebb67050c815fc0cefee4d947b30fab8ff5517
lozhenru <zhenru@ndrmedical.com>
2023-05-15 00:41:51 +0800

    changes tab design

--- src/blueprints/content_manager/static/js/content_manager.js
+++ src/blueprints/content_manager/static/js/content_manager.js
@@ -9,11 +9,32 @@ $(function() {
         }
     };
 
+    function open_content(id){
+        $(".center_content_container").removeClass("selected_content");
+        if (id === "row_create_content" && $("#"+id).hasClass("selected_row")){
+            $("#center_create_content").toggleClass("selected_content");
+        }
+        else if (id === "row_custom_field" && $("#"+id).hasClass("selected_row")){
+            $("#center_custom_field").toggleClass("selected_content");
+        }
+        else if (id === "row_create_page" && $("#"+id).hasClass("selected_row")){
+            $("#center_create_page").toggleClass("selected_content");
+        }
+    }
+
     $(".left_panel_tab").on("click", function(event){
         event.preventDefault();
+        var current_tab = $(this);
         $(".left_panel_tab").removeClass("selected_tab")
-        $(this).toggleClass("selected_tab");
+        current_tab.toggleClass("selected_tab");
         open_menu($(this).attr("id"));
+    });
+
+    $(".left_panel_menu_row").on("click", function(event){
+        event.preventDefault();
+        $(".left_panel_menu_row").removeClass("selected_row");
+        $(this).toggleClass("selected_row");
+        open_content($(this).attr("id"));
     });
 
     $("#content_manager_anchor").toggleClass("current_anchor");
